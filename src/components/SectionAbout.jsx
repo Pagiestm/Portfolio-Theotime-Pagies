4b1ebed40889d870b483f5da14f08fc78681b2ff
pagiestm <pagiestm@gmail.com>
2024-09-02 12:07:48 +0200

    fix(SectionAbout, SectionPortfolio): remove sparkles

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -36,17 +36,6 @@ console.log(greet('tout le monde'));`;
           <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
           <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
           <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
-
-          <SparklesCore
-            background="transparent"
-            minSize={0.4}
-            maxSize={1}
-            particleDensity={1200}
-            className="w-full h-full"
-            particleColor="#FFFFFF"
-          />
-
-          <div className="absolute inset-0 bg-gray-800 w-full h-full [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
         </div>
       </div>
       <p className="text-lg max-w-2xl text-center mb-10 text-white mt-6">
--- src/components/SectionPortfolio.jsx
+++ src/components/SectionPortfolio.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { SparklesCore } from "./ui/sparkles";
 
 const SectionPortfolio = () => {
   return (
-    <section id="portfolio" className="flex flex-col justify-center items-center min-h-screen p-10 bg-gray-900">
-      <div className="bg-gray-900 w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
+    <section id="portfolio" className="flex flex-col justify-center items-center min-h-screen p-10 bg-gradient-to-b from-gray-800 to-gray-900">
+      <div className="w-full flex flex-col items-center justify-center overflow-hidden rounded-md">
         <h1 className="md:text-4xl text-3xl lg:text-5xl font-bold text-center text-white relative z-20">
           Mes projets
         </h1>
@@ -13,17 +12,6 @@ const SectionPortfolio = () => {
           <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
           <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
           <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
-
-          <SparklesCore
-            background="transparent"
-            minSize={0.4}
-            maxSize={1}
-            particleDensity={1200}
-            className="w-full h-full"
-            particleColor="#FFFFFF"
-          />
-
-          <div className="absolute inset-0 bg-gray-900 w-full h-full [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-6">
