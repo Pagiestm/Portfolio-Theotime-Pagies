b285aa70b2d987d71aa11583d5fc4febc92dcec5
pagiestm <pagiestm@gmail.com>
2024-09-04 16:42:05 +0200

    style(Footer): add mt in mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center p-4 bg-gray-900 text-gray-500">
+    <footer className="flex justify-center items-center p-4 bg-gray-900 text-gray-500 mt-10 md:mt-0">
       <p>&copy; 2024 Théotime Pagies. Tous droits réservés.</p>
     </footer>
   );
